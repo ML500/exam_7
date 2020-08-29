1e4f905c360ef20a15a4ce847a6c99934e947247
Emil <emilrakaev@gmail.com>
2020-08-29 12:03:53 +0600

    добавил админку, создал и  зарегистрировал модели Poll и Choice в админ-панели

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Poll, Choice
 
-# Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'created_at')
+    list_display_links = ('pk', 'text')
+    search_fields = ('text',)
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    text = models.TextField(max_length=3000, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=3000, verbose_name='Ответ')
+    poll = models.ForeignKey('webapp.Poll', related_name='choices',
+                             on_delete=models.CASCADE, verbose_name='Опрос')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Вариант ответов'
